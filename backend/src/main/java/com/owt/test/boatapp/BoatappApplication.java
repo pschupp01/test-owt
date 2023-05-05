0b1141dca9bb899af54ab192f511bfa888b8a279
Paul SCHUPP <paul.schupp01@gmail.com>
2023-05-05 17:28:08 +0200

    Backend: Initialize Spring app

---
+++ backend/src/main/java/com/owt/test/boatapp/BoatappApplication.java
@@ -0,0 +1,13 @@
+package com.owt.test.boatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/owt/test/boatapp/BoatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owt.test.boatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
