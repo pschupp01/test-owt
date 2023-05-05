307d7f9dc2d2813cf369b563d4495cf802ac1463
Paul SCHUPP <paul.schupp01@gmail.com>
2023-05-05 22:31:24 +0200

    Backend: setup CORS Policy

--- backend/src/main/java/com/owt/boatapp/config/SecurityConfiguration.java
+++ backend/src/main/java/com/owt/boatapp/config/SecurityConfiguration.java
@@ -1,12 +1,19 @@
 package com.owt.boatapp.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     public SecurityConfiguration() {
@@ -16,14 +23,24 @@ public class SecurityConfiguration {
     public SecurityFilterChain boatFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .securityMatchers((matchers) -> matchers
- 				    .requestMatchers("/boats")
-                )
-	  			.authorizeHttpRequests()
+                .securityMatchers((matchers) -> matchers.requestMatchers("/boats"))
+                .authorizeHttpRequests()
                 .anyRequest()
                 .permitAll();
 
         return http.build();
     }
+
+    @Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
 }
