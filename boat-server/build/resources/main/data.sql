64932f17dba7b06ee0b2d459b4d6f40d8da4d115
Paul SCHUPP <paul.schupp01@gmail.com>
2023-05-08 15:05:18 +0200

    rename server folder

---
+++ boat-server/build/resources/main/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Boat (name, description) VALUES ('Titanic', 'A big boat');
+INSERT INTO Boat (name, description) VALUES ('Santa Maria', 'An old boat');
+INSERT INTO Boat (name, description) VALUES ('France', 'A classy boat');
+
+INSERT INTO User_Info (username, password) VALUES ('admin', '$2a$10$s515CmBbRNGa9o.z5Bel1eU1lbvhgoFf6eyWTUqMjGvK0GpfCxoNq');
